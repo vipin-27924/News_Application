1c5c2c8ff4b58a7811101aae66034d00cd451ed3
vipin kumar <vipin.k27924@gmail.com>
2026-01-03 14:19:33 +0530

    Improved The UI

--- app/src/main/java/com/example/news/ui/components/HomeNavBar.kt
+++ app/src/main/java/com/example/news/ui/components/HomeNavBar.kt
@@ -1,4 +1,46 @@
 package com.example.news.ui.components
 
-class HomeNavBar {
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Whatshot
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.news.R
+
+sealed class NavScreen(val route: String, val title: String, val icon: ImageVector) {
+    object Home : NavScreen("home", "Home", Icons.Default.Home)
+    object Trending : NavScreen("trending", "Trending", Icons.Default.Whatshot)
+    object Saved : NavScreen("saved", "Saved", Icons.Default.Bookmark)
+}
+
+@Composable
+fun HomeNavBar() {
+    val items = listOf(NavScreen.Home, NavScreen.Trending, NavScreen.Saved)
+    var selectedItem by remember { mutableIntStateOf(0) }
+
+    NavigationBar(
+        containerColor = Color.White,
+        modifier = Modifier.navigationBarsPadding(),
+        tonalElevation = 8.dp
+    ) {
+        items.forEachIndexed { index, screen ->
+            NavigationBarItem(
+                icon = { Icon(screen.icon, contentDescription = screen.title) },
+                label = { Text(screen.title) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = colorResource(id = R.color.ActionIndigo),
+                    indicatorColor = colorResource(id = R.color.BackgroundMain)
+                )
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/news/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/news/ui/home/HomeScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -19,14 +21,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.news.ui.components.NewsCard
 import com.example.news.ui.components.NewsTopBar
 import com.example.news.data.remote.dto.NewsUiState
 import com.example.news.data.remote.dto.NewsViewModel
+import com.example.news.ui.components.HomeNavBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewsHomeScreen(viewModel: NewsViewModel = viewModel()) {
+
+fun NewsHomeScreen(viewModel: NewsViewModel = viewModel(),navController: NavHostController = rememberNavController()) {
 
 
 
@@ -57,6 +65,23 @@ fun NewsHomeScreen(viewModel: NewsViewModel = viewModel()) {
                         Spacer(modifier = Modifier.height(20.dp))
                     }
                 }
+                Scaffold(
+                    bottomBar = {
+                        // We pass the controller to the NavBar so it can tell the app to move
+                        HomeNavBar(navController = navController)
+                    }
+                ) { innerPadding ->
+                    // The NavHost is where the actual page swapping happens
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("home") { HomeScreen() }
+                        composable("trending") {  }
+                        composable("saved") {  }
+                    }
+                }
             }
         }
         is NewsUiState.Error -> {
@@ -65,4 +90,9 @@ fun NewsHomeScreen(viewModel: NewsViewModel = viewModel()) {
             }
         }
     }
+}
+
+@Composable
+fun HomeScreen() {
+    TODO("Not yet implemented")
 }
